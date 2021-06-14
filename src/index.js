c4dc194fec10adee49ff129be54a80bf824b3c11
marlonaesparza <marlon@DESKTOP-6P13I3V.localdomain>
2021-06-14 11:10:36 -0700

    Cleaned Up Files: Preparing for advancement.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import App from "./scripts/App.js";
-import { samples, colors } from "./scripts/config.js"
+import { samples, colors } from "./config.js"
 
 const root = document.getElementById("root");
 const app = new App("marlon", samples, colors).render();
