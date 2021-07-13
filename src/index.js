348b4e5e1a8f6d7c6b6ab2bb0b04e626e1929ef4
marlonaesparza <marlon@DESKTOP-6P13I3V.localdomain>
2021-07-12 18:39:23 -0700

    Installed and Imported: regenerator-runtime/runtime

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+import 'regenerator-runtime/runtime';
 import App from "./scripts/App.js";
-// import * as Tone from 'tone';
+import * as Tone from 'tone';
 import { notes } from "./config.js"
 
 const root = document.getElementById("root");
-const app = new App({notes}).render();
+const app = new App({notes, Tone}).render();
 
 root.appendChild(app);
